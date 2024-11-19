fab5df105716b7e5bfe2e9d74067419855e2a174
Guilherme Dias <guilhermediasthomaz@hotmail.com>
2024-11-18 21:09:42 -0300

    Implementando classe para tratar exceções de validação

---
+++ LoansChallenge/src/main/java/com/guilherme/LoansChallenge/exceptions/ValidationsExceptions.java
@@ -0,0 +1,8 @@
+package com.guilherme.LoansChallenge.exceptions;
+
+public class ValidationsExceptions extends RuntimeException {
+
+    public ValidationsExceptions(String message) {
+        super(message);
+    }
+}
